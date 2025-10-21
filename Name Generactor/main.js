d52f0258f62e01a49accc2fcd273c2fcac26b1be
raghulprabu <raghulitsme23@gmail.com>
2025-10-22 00:41:20 +0530

    some task pushing

---
+++ Name Generactor/main.js
@@ -0,0 +1,17 @@
+let inputbox = document.getElementById("inputbox");
+let result = document.getElementById("result");
+
+const update = () => {
+  result.textContent = inputbox.value;
+};
+
+
+
+
+let inputbox2 = document.getElementById("inputbox2");
+let result2 = document.getElementById("result2");
+
+const update2 = () => {
+  result2.textContent = inputbox2.value;
+};
+
